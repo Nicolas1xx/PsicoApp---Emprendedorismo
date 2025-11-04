b4731fdc2812c131837b4e1b417f84d8574f76b4
Nicolas1xx <nicolas.leao.senai@gmail.com>
2025-11-04 16:22:46 -0300

    ok

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\SESI SENAI 2\Documents\psicoapp\venv
+set VIRTUAL_ENV=C:\Users\SESI SENAI 2\Documents\2025\SITES E PROJETOS\PROJETOS INDIVIDUAIS\PsicoApp - Emprendedorismo\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
